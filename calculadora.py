a0fa4d022d7cee702e42a38db4a0a26ee991f8b6
JUNIOR SANTOS <tst.juniorsantos@gmail.com>
2025-08-30 16:08:00 -0300

    Calculadora pcd
    Calcula soma, subtração, multiplicação e divisão

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+operador = input("Digite A operação entre +, -, *, /: ")
+num1 = int(input(" Descreva o primeiro número inteiro: ")))+
+num2 = int(input(" Descreva o prisegundo número inteiro: "))
+
+if operador == "+":
+    print(f"O resultado da soma é: {num1 + num2}")
+
+elif operador == "-":
+    print(f"O resultado da subtração é: {num1 - num2}")
+
+elif operador == "*":
+    print(f"O resultado da multiplicação é: {num1 * num2}")
+
+elif operador == "/":
+    print(f"O resultado da divisão é: {num1 / num2}")
+        
